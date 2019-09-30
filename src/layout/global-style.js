a0c9545a36e1bde94632e82f8e5ee619923a245f
Krystal Klumpp <kklumpp13@gmail.com>
2019-09-29 18:22:14 -0600

    Add Google fonts

--- src/layout/global-style.js
+++ src/layout/global-style.js
@@ -25,10 +25,12 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     background-color: white;
+    font-family: 'Roboto', sans-serif;
   }
 
   h1, h2, h3, h4, h5, h6 {
     color: #222;
+    font-family: 'Fira Sans', sans-serif;
   
     + * + {
       margin-top: 0.5rem;
